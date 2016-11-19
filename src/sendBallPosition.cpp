596e3c547f61b06210eb9e0062e3e2d65823f823
kzts <kazutoshi.tanaka@gmail.com>
2016-11-19 12:13:01 +0900

    use cammera

--- src/sendBallPosition.cpp
+++ src/sendBallPosition.cpp
@@ -1,22 +1,30 @@
+//etc
+#include <stdio.h>
+#include <sys/time.h>
+#include <iostream>
+#include <fstream>
+//opencv
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-#include <fstream>
-
 // socket
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
-
 #include <arpa/inet.h>
 
 using namespace std;
 using namespace cv;
 //double PosBall[120*15][2];
 
+#define CAMERA_WIDTH 640
+#define CAMERA_HEIGHT 480
+#define CAMERA_FPS 60
+#define AVI_FILE 0
+#define CAMERA 1
+#define END_TIME_SEC 10
 #define XY 2
 
 double pos_ctr[XY];
@@ -25,6 +33,7 @@ int params_roi[XY+XY];
 unsigned int threshold_gray = 50; 
 unsigned int len_roi = 30; 
 Mat old_img, dst_img;
+int view_mode = 0;
 
 /*
 void saveBall(void){
@@ -94,12 +103,15 @@ void drawCircle( Mat smt_img_ ){
     
     vector<Vec3f> circles;            
     HoughCircles( roi, circles, CV_HOUGH_GRADIENT, 1, 100, 6, 20 ); // detect circle
-    
-    for( size_t i = 0; i < circles.size(); i++ ){
-      Point center( cvRound( circles[i][0] + params_roi[0] ), cvRound( circles[i][1] + params_roi[1] ));
-      int radius =  cvRound(circles[i][2]);
-      circle( dst_img, center, 3, Scalar(0,255,0), -1, 8, 0 );
-      circle( dst_img, center, radius, Scalar(0,0,255), 3, 8, 0 );
+
+    if ( view_mode > 0 ){    
+      for( size_t i = 0; i < circles.size(); i++ ){
+	Point center( cvRound( circles[i][0] + params_roi[0] ), 
+		      cvRound( circles[i][1] + params_roi[1] ));
+	int radius =  cvRound(circles[i][2]);
+	circle( dst_img, center, 3, Scalar(0,255,0), -1, 8, 0 );
+	circle( dst_img, center, radius, Scalar(0,0,255), 3, 8, 0 );
+      }
     }
     if ( circles.size() > 0 ){
       bal_ctr[0] = circles[0][0] + params_roi[0];
@@ -124,16 +136,22 @@ void getCircleCenter( Mat src_img_ ){
 }
 
 
-int main(int argc, char* argv[]){
-  // opencv
-  VideoCapture cap(argv[1]);
-  Mat src_img, gry_img;
-
-  namedWindow( "dst", CV_WINDOW_AUTOSIZE);
-
-  cap >> src_img;
-  cvtColor( src_img, gry_img, CV_BGR2GRAY );
-  old_img = gry_img.clone();
+int main(int argc, char* argv[])
+{
+  // command line input
+  if ( argc != 5 ){
+    cout << "input four: " 
+	 << "view mode (off:0, on:1), " 
+	 << "source (load:0, camera:1), " 
+	 << "IP address and " 
+	 << "video name or camera number." 
+	 << endl;
+    return -1;
+  }
+  view_mode =atoi(argv[1]);
+  int source = atoi(argv[2]);
+  char* ip_address = argv[3];
+  //char* source = argv[4];
 
   // server
   int welcomeSocket, newSocket;
@@ -145,7 +163,8 @@ int main(int argc, char* argv[]){
   welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(7891);
-  serverAddr.sin_addr.s_addr = inet_addr("192.168.2.247");
+  //serverAddr.sin_addr.s_addr = inet_addr("192.168.2.247");
+  serverAddr.sin_addr.s_addr = inet_addr(ip_address);
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
   bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
@@ -157,26 +176,76 @@ int main(int argc, char* argv[]){
   addr_size = sizeof serverStorage;
   newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
 
-  //strcpy(buffer,"Hello World\n");
-  //send(newSocket,buffer,13,0);
-
-  // loop
-  while (true){
-    //load
+  // opencv
+  if ( source == AVI_FILE ){
+    char* avi_filename = argv[4];
+    VideoCapture cap(avi_filename); 
+    Mat src_img, gry_img;
+    // initiation 
+    if ( view_mode > 0 )
+      namedWindow( "dst", CV_WINDOW_AUTOSIZE);
     cap >> src_img;
-    if ( src_img.empty() )
-      break;
-    getCircleCenter( src_img );
-    imshow( "dst", dst_img );
-    
-    //sprintf( buffer, "%d %d", (int) pos_ctr[0], (int) pos_ctr[1] );
-    sprintf( buffer, "%d %d", (int) bal_ctr[0], (int) bal_ctr[1] );
-    send( newSocket, buffer, 13, 0);
+    cvtColor( src_img, gry_img, CV_BGR2GRAY );
+    old_img = gry_img.clone();
+    // loop
+    while (true){
+      cap >> src_img;
+      if ( src_img.empty() )
+	break;
+      getCircleCenter( src_img );
+      
+      if ( view_mode > 0 )
+	imshow( "dst", dst_img );
+      
+      //sprintf( buffer, "%d %d", (int) pos_ctr[0], (int) pos_ctr[1] );
+      sprintf( buffer, "%d %d", (int) bal_ctr[0], (int) bal_ctr[1] );
+      send( newSocket, buffer, 13, 0);
  
-    waitKey(1);
-    //waitKey(100);
+      waitKey(1);
+    }
   }
-  
+  if ( source == CAMERA ){
+    struct timeval ini, now;
+    gettimeofday(&ini, NULL);
+
+    int camera_num = atoi(argv[4]);
+    VideoCapture cap(camera_num); 
+    if(!cap.isOpened()){
+      cout << "camera is not found." << endl;
+      return -1;
+    }else{
+      cap.set( CV_CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH );
+      cap.set( CV_CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT );
+      cap.set( CV_CAP_PROP_FPS, CAMERA_FPS );
+    }
+    Mat src_img, gry_img;
+    // initiation 
+    if ( view_mode > 0 )
+      namedWindow( "dst", CV_WINDOW_AUTOSIZE);
+    cap >> src_img;
+    cvtColor( src_img, gry_img, CV_BGR2GRAY );
+    old_img = gry_img.clone();
+    // loop
+    while (true){
+      gettimeofday(&now, NULL);
+
+      cap >> src_img;
+      if ( now.tv_sec - ini.tv_sec > END_TIME_SEC )
+	break;
+      getCircleCenter( src_img );
+      
+      if ( view_mode > 0 )
+	imshow( "dst", dst_img );
+      
+      //sprintf( buffer, "%d %d", (int) pos_ctr[0], (int) pos_ctr[1] );
+      sprintf( buffer, "%d %d", (int) bal_ctr[0], (int) bal_ctr[1] );
+      send( newSocket, buffer, 13, 0);
+ 
+      waitKey(1);
+    }
+  }
+
+  // close socket  
   strcpy( buffer,"END");
   send( newSocket, buffer, 13, 0);
 
